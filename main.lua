d10e8b5b5c3e01752d4288dc12f0116b523ee575
Samuel Mauldin <Sam@Sams-Laptop.local>
2013-06-25 18:21:11 -0500

    silly me

--- main.lua
+++ main.lua
@@ -74,7 +74,7 @@ local disp = glass.addBox(20, 20, 136, 48, col.gray, 0.7)
 
 local title = glass.addText(75, 25, "", col.textGray)
 title.setZIndex(5)
-local main = plastic.addText(40, 27, "", gColors.blue)
+local main = glass.addText(40, 27, "", gColors.blue)
 main.setZIndex(5)
 
 setText("Tent", title)
