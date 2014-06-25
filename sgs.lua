1b628512ae04b4d8cc53c7e4e2335e782730c248
Sam Mauldin <sam@fluidnode.com>
2014-06-25 16:06:59 -0500

    Update sgs.lua

--- sgs.lua
+++ sgs.lua
@@ -1,19 +1,3 @@
 {
-	["sxw"] = {"Sxw1212", "SKFANFA"},
-	["sxw2"] = {"Sxw1212-2", "DIAANFA"},
-	["sxwfb"] = {"Sxw1212-fb", "BAJDNBA"},
-	["snake"] = {"Snakybo", "BQPANFA"},
-	["master"] = {"Master", "GIAANFA"},
-	["fun"] = {"Funmaker""RKFANFA"},
-	["karel"] = {"Karelmikie", "NXEBNFA"},
-	["mud"] = {"MudkipTheEpic", "YKFANFA"},
-	["wii"] = {"Wiiplay", "HFUBNFA"},
-	["std"] = {"SuicidialSTDz", "UXFZMFA"},
-	["eor"] = {"eor", "SIBNOBA"},
-	["ds"] = {"ds", "CDQZMFA"},
-	["casino"] = {"Mark-Casino", "CESVMBA"},
-	["gold"] = {"GoldenBoy", "RNMLOBA"},
-	["1lann"] = {"1lann", "SDSVMBA"},
-	["pixel"] = {"PixelToast", "LEXVMBA"},
-	["tripy"] = {"tripy", "APOSMBA"}
+	["sxw"] = {"Sxw1212", "CQPANBA"}
 }
