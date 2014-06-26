68de16e240152af001db8b3e8d5ac2e0a9471c2a
Sam Mauldin <sam@fluidnode.com>
2014-06-26 12:13:55 -0500

    Update sgs.lua

--- sgs.lua
+++ sgs.lua
@@ -2,5 +2,6 @@
 	["sxw"] = {"Sxw1212", "CQPANBA"},
 	["seth"] = {"sethxia", "GQQYMBA"},
 	["spawn"] = {"spawn", "HIAANBA"},
-	["sertexbank"] = {"bank", "GNKANBA"}
+	["sertexbank"] = {"bank", "GNKANBA"},
+	["sertexlabs"] = {"sertex", "KNHANBA"}
 }
